e720cf462ce24816bf32274d8a371b017199df13
Kevin <kevin.billet.pro@gmail.com>
2019-11-28 10:22:15 +0100

    add commentary

--- exercise2/main.py
+++ exercise2/main.py
@@ -3,5 +3,5 @@ from city import *
 from cities import *
 
 for i in cities :
-    i = City(i)
-    City.show_information(i)
+    i = City(i) # create object with i
+    City.show_information(i) # display informations of the city
