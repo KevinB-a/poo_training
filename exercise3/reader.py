94d40df4d8c2a4a75a8d554427a8fa8742c33cd5
Kevin <kevin.billet.pro@gmail.com>
2019-11-29 16:42:47 +0100

    exercise3

---
+++ exercise3/reader.py
@@ -0,0 +1,45 @@
+# coding: utf8
+from book import *
+
+class Reader():
+    """ """
+    def __init__(self):
+        self.book = None
+        self.title = None
+        self.pages = None
+
+    def borrow_book(self, Library, title):
+        """method for borrow a book """
+
+        get_book()
+        raise
+
+    def go_to_page(self, page):
+        """ """
+        self.page = page
+        if current_page < page  :
+            current_page = page
+
+
+    def next_page(self):
+        """method for turn to the next page """
+        current_page +=1
+
+
+    def previous_page(self):
+        """method for go back to the previous page """
+        current_page -= 1
+
+
+    def read(self):
+        """ """
+        print("you read the page ")
+
+
+    def read_book(self):
+        """method for read the whole book """
+        self.pages
+        current_page = -1
+        while current_page < :
+            current_page += 1
+        return current_page
